16d131729720babec22722f70478d5071edb5a20
mordauk <mordauk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-20 13:14:34 +0000

    Tagging v1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/github-contributors/tags/1.0.1@587766 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ github-contributors.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+Plugin Name: Github Contributors Short Code
+Plugin URI: http://pippinsplugins.com/
+Description: Displays a list of all contributors of any project/repo on Github
+Author: Pippin Williamson
+Author URI: http://pippinsplugins.com
+Contributors: mordauk
+Version: 1.0.1
+*/
+
+
+function pw_get_github_contributors( $atts, $content = null ) {
+	extract( shortcode_atts( array(
+			'username' => 'Automattic',
+			'repo'     => '_s'
+		), $atts )
+	);
+
+	$transient_key = md5( 'pw_gh_contribs' . print_r( $atts, true ) );
+
+	$contributors = get_transient( $transient_key );
+	if ( false === $contributors )
+		$contributors = pw_get_github_contributors_query( $username, $repo, $transient_key );
+
+	if ( ! is_array( $contributors ) )
+		return '';
+
+	$contrib_list = sprintf( '<div id="pw_github_contributors" class="pw_gh_%s">',
+		esc_attr( strtolower( $username ) . '_' . strtolower( str_replace('-', '_', $repo ) ) )
+	);
+
+	foreach ( $contributors as $contributor ) {
+		$contrib_list .= '<div class="pw_gh_contributor" style="width: 120px; display: inline-block;">';
+		$contrib_list .= sprintf( '<a href="%s" title="%s">',
+			esc_url( 'https://github.com/' . $contributor->login ),
+			esc_html( sprintf( __('View %s', 'pw_github'), $contributor->login ) )
+		);
+		$contrib_list .= sprintf( '<img src="%s" width="80" height="80"/>', esc_url( $contributor->avatar_url ) );
+		$contrib_list .= sprintf( '<p class="pw_gh_name">%s</p>', esc_html( $contributor->login ) );
+		$contrib_list .= '</a>';
+		$contrib_list .= '</div>';
+	}
+
+	$contrib_list .= '</div>';
+
+	return $contrib_list;
+}
+add_shortcode( 'github_contributors', 'pw_get_github_contributors' );
+
+function pw_get_github_contributors_query( $username, $repo, $transient_key ) {
+	$response = wp_remote_get( "https://api.github.com/repos/{$username}/{$repo}/contributors" );
+
+	if ( is_wp_error( $response ) ) {
+		set_transient( $transient_key, '', 3600 );
+		return flase;
+	}
+
+	$contributors = json_decode( wp_remote_retrieve_body( $response ) );
+	if ( ! is_array( $contributors ) ) {
+		set_transient( $transient_key, '', 3600 );
+		return false;
+	}
+
+	set_transient( $transient_key, $contributors, 3600 );
+	return $contributors;
+}
---
+++ readme.txt
@@ -0,0 +1,58 @@
+=== Github Contributors ===
+Author URI: http://pippinsplugins.com
+Plugin URI: http://pippinsplugins.com/github-contributors
+Contributors: mordauk
+Donate link: http://pippinsplugins.com/support-the-site
+Tags: github, contributors, mordauk, Pippin Williamson, pippinsplugins
+Requires at least: 3.2
+Tested up to: 3.4.1
+Stable Tag: 1.0.1
+
+Provides a simple short code for displaying a list of contributors from any Github repository.
+
+== Description ==
+
+This plugin provides a short code that you can use for showing a photo grid of all users that have contributed code to any Github repository.
+
+It's a great way to show off the people that have helped with your project.
+
+You can see this live on the Contributors page for [Easy Digital Downloads](http://easydigitaldownloads.com/contributors/).
+
+Based on the code by [Konstantin Kovshenin](http://kovshenin.com/2012/how-to-get-a-list-of-contributors-from-a-github-project/).
+
+== Installation ==
+
+1. Upload to wp-content/plugins/
+2. Activate the plugin
+3. Place [github-contributors username="{YOUR USERNAME}" repo="{NAME OF YOUR REPO}"] on any page
+
+
+== Screenshots ==
+
+1. Screenshot 1
+
+== Changelog ==
+
+= 1.0.1 =
+
+* Updated the transient key to use an MD5 to avoid it breaking if the repo / username is too long
+* Escaped a couple of possibly unsafe values
+* Updated some formatting with indention, whitespace, double quotes
+* Failed responses are now cached
+
+= 1.0 =
+
+* First offical release!
+
+== Upgrade Notice ==
+
+= 1.0.1 =
+
+* Updated the transient key to use an MD5 to avoid it breaking if the repo / username is too long
+* Escaped a couple of possibly unsafe values
+* Updated some formatting with indention, whitespace, double quotes
+* Failed responses are now cached
+
+= 1.0 =
+
+* First offical release!
